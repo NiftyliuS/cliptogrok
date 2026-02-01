9a14aae0d0a5d969d57320bbfe029be99e9034c9
CLUNKER\vladi <niftylius@gmail.com>
2026-02-01 21:37:55 +0200

    adding reproducable setup for the baseline AdamW

--- train.py
+++ train.py
@@ -269,3 +269,27 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     main(args)
+
+# Default setup - no norm no clip ~35k steps to 95% val
+# if __name__ == "__main__":
+#     parser = ArgumentParser()
+#     parser.add_argument("--p", type=int, default=97)
+#     parser.add_argument("--budget", type=int, default=3e5)
+#     parser.add_argument("--batch_size", type=int, default=512)
+#     parser.add_argument("--weight_decay", type=float, default=1)
+#     parser.add_argument("--train_ratio", type=float, default=0.5)
+#
+#     # Optimizer controls
+#     parser.add_argument("--optimizer", default="AdamW")
+#     parser.add_argument("--lr", type=float, default=1e-3)
+#     parser.add_argument("--beta1", type=float, default=0.9)
+#     parser.add_argument("--beta2", type=float, default=0.98)
+#
+#     # Clip to Grok specific arguments
+#     parser.add_argument("--random_seed", type=bool, default=False)
+#     parser.add_argument("--seed", type=int, default=0)
+#     parser.add_argument("--init_norm", type=float, default=0.0)  # 0 = disable
+#     parser.add_argument("--max_norm", type=float, default=0.0)  # 0 = disable
+#
+#     args = parser.parse_args()
+#     main(args)
