ae365513f6c4795dc1c8adf2815314b48e5d6832
CLUNKER\vladi <niftylius@gmail.com>
2026-07-14 01:01:51 +0300

    adding experiment setups and results

---
+++ goblin_tests/llama_adam_all_mod_noclip.py
@@ -0,0 +1,37 @@
+from argparse import ArgumentParser
+from train_goblin_vs_llama import main
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--model", type=str, default="llama", choices=["goblin", "llama"])
+
+    parser.add_argument("--task", type=str, default="all-mod",
+                        choices=["add-p97", "sub-p97", "mul-p97", "div-p97", "all-mod", "S5"])
+    parser.add_argument("--budget", type=int, default=5e3)  # 5_000 steps
+    parser.add_argument("--batch_size", type=int, default=2048)  # 4 times the data, 4 times the batch size
+    parser.add_argument("--train_ratio", type=float, default=0.5)
+
+    # Model configuration
+    parser.add_argument("--dim", type=int, default=128)
+    parser.add_argument("--num_layers", type=int, default=2)
+    parser.add_argument("--num_heads", type=int, default=4)
+
+    # Optimizer controls
+    parser.add_argument("--optimizer", default="Adam")
+    parser.add_argument("--weight_decay", type=float, default=0.01)
+    parser.add_argument("--lr", type=float, default=1e-3)
+    parser.add_argument("--beta1", type=float, default=0.9)
+    parser.add_argument("--beta2", type=float, default=0.95)
+
+    # Seed options
+    parser.add_argument("--random_seed", action="store_true")
+    parser.add_argument("--seed", type=int, default=0)
+
+    # Clip controls
+    parser.add_argument("--init_pattern", type=str, default="all", choices=["all", "edge"])
+    parser.add_argument("--init_norm", type=float, default=0.0)  # 0 = disable
+    parser.add_argument("--max_norm", type=float, default=0.0)  # 0 = disable
+
+    parser.add_argument("--plot_progress", action="store_true")
+    args = parser.parse_args()
+    main(args)
